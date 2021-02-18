e618cadfeeada7dc51ecd7351b5e08ef3db28991
Selena Singleton <ssingle7@gmail.com>
2021-02-17 22:15:06 -0500

    ran server to troubleshoot errors rendered in browser

--- src/components/EmployeeTable/index.jsx
+++ src/components/EmployeeTable/index.jsx
@@ -4,15 +4,15 @@ import React from "react";
 const EmployeeTable = (props) => {
     return (
         <table className="table table-striped table-sortable text-center">
-        <thead>
-          <tr>
-            <th scope="col">Image</th>
-            <th scope="col" data-field="name" data-sortable="true">
-                <span onClick={() => props.sortBy("name", "last", "first")}>
-                    Name 
-                    </span>
-                    </th>
-                    <th scope="col">
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col" data-field="name" data-sortable="true">
+            <span onClick={() => props.sortBy("name", "last", "first")}>
+              Name
+            </span>
+          </th>
+          <th scope="col">
             <span onClick={() => props.sortBy("phone")}>Phone</span>
           </th>
           <th scope="col">
@@ -23,12 +23,34 @@ const EmployeeTable = (props) => {
           </th>
         </tr>
       </thead>
-        </tbody>
-      </table>
-    );
+      <tbody>
+        {props.state.filteredEmployees.map((employee) => {
+          const { first, last } = employee.name;
+          const fullName = `${first} ${last}`;
+
+          // Format date
+          const dob = props.formatDate(employee.dob.date);
+
+          return (
+            <tr key={employee.login.uuid}>
+              <td>
+                <img src={employee.picture.thumbnail} alt={fullName} />
+              </td>
+              <td className="align-middle">{fullName}</td>
+              <td className="align-middle">
+              <a href={`tel:+1${employee.phone}`}>{employee.phone}</a></td>
+              <td className="align-middle email">
+                <a href={`mailto:${employee.email}`}>{employee.email}</a>
+              </td>
+              <td className="align-middle">{dob}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default EmployeeTable;
-
 
 
