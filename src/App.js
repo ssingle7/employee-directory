2f2de45ef96c1ae7867669a3a91190409026c0b6
Selena Singleton <ssingle7@gmail.com>
2021-02-18 00:15:31 -0500

    created methods to render the class components to display the employee table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"; 
 // import "./App.css"; 
-import Header from "./components/Header"; 
+import Header from "./components/Header/Header"; 
 import EmployeesContainer from "./components/container/EmployeesContainer";
 
 const App = () => {
